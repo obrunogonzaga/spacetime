a37358066a38b6b894d6fb98b58d9dd5326463f9
Bruno Gonzaga Santos <brunog86@gmail.com>
2023-06-09 19:50:11 +0000

    inital commit - Create structure of projects (backend and frontend)

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
